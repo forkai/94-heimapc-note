5a4f3e123647818e089d9011a78f5d1c144b5f6f
gaoly <shuiruohanyu@foxmail.com>
2020-02-28 15:10:22 +0800

    插槽

---
+++ 插槽示例/src/router/index.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+Vue.use(VueRouter) // 全局注册
+import defaultSlot from '../views/defaultslot'
+import bakSlot from '../views/bakslot'
+export default new VueRouter({
+    routes: [{
+        path: '/',
+        redirect: '/defaultSlot' // 强制跳转到 默认插槽
+    }, {
+        path: '/defaultSlot',
+        component: defaultSlot
+    }, {
+        path: '/bakSlot',
+        component: bakSlot
+    }]
+})
+//  导出实例
